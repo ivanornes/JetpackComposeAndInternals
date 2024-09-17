73105a83ccb540bb3a30483effa4892bd91ec495
ivan ornes <ivanornes@gmail.com>
2024-09-17 23:03:23 +0200

    Solution to exercise 3

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SpeakersScreen.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SpeakersScreen.kt
@@ -4,6 +4,7 @@ package dev.jorgecastillo.compose.app.ui.composables
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,10 +13,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -25,6 +28,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -40,12 +44,58 @@ import dev.jorgecastillo.compose.app.ui.theme.ComposeAndInternalsTheme
 
 @Composable
 fun SpeakersScreen(speakers: List<Speaker>) {
+Scaffold(
+    topBar = {
+        TopAppBar(title = {
+            Text("Speakers")
+        })
+    },
+    floatingActionButton = {
+        FloatingActionButton(onClick = {
+        }) {
+            Icon(
+                Icons.Filled.Add,
+                contentDescription = "Add new speaker",
+                tint = Color.Black
+            )
+        }
 
+}, content = { paddingValues ->
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .verticalScroll(rememberScrollState())
+            .testTag("SpeakersList")
+    ) {
+        speakers.forEach {
+            SpeakerCard(it, onClick = { _ ->
+                
+            })
+        }
+    }
+    })
 }
 
 @Composable
 fun SpeakerCard(speaker: Speaker, onClick: (Speaker) -> Unit = {}) {
-    
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(dimensionResource(R.dimen.spacing_small))) {
+            Row(modifier = Modifier.padding(dimensionResource(R.dimen.spacing_regular))) {
+                Image(
+                    painter = painterResource(avatarResForId(speaker.id)),
+                    contentDescription = "Speaker image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.avatar_size))
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(dimensionResource(R.dimen.spacing_regular))) {
+                    Text(speaker.name, style = MaterialTheme.typography.h6)
+                    Text(speaker.company, style = MaterialTheme.typography.caption)
+                }
+            }
+    }
 }
 
 @SuppressLint("DiscouragedApi")
